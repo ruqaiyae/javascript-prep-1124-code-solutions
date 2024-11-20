ffc3f573ad05a664d87451b66c7944741db1e5b3
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2024-11-20 19:07:43 +0000

    Completed prep-javascript-operators-and-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const number1 = 5;
+const number2 = 2;
+const product = number1 * number2;
+console.log('value of product: ' + product);
+console.log('typeof product: ' + typeof product);
+
+const charge = 20;
+const payment = 18;
+const amountRemaining = charge - payment;
+console.log('value of amountRemaining: ' + amountRemaining);
+console.log('typeof amountRemaining: ' + typeof amountRemaining);
+
+const tests = 45;
+const assignments = 25;
+const final = 80;
+const grade = (tests + assignments + final) / 3;
+console.log('value of grade: ' + grade);
+console.log('typeof grade: ' + typeof grade);
+
+const firstName = 'Ruqaiya';
+const lastName = 'Electricwala';
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ' + fullName);
+console.log('typeof fullName: ' + typeof fullName);
+
+const pH = 5.5;
+const isAcidic = pH < 7;
+console.log('value of isAcidic: ' + isAcidic);
+console.log('typeof isAcidic: ' + typeof isAcidic);
+
+const numberOfSoldiers = 18;
+const isSparta = numberOfSoldiers === 300;
+console.log('value of isSparta: ' + isSparta);
+console.log('typeof isSparta: ' + typeof isSparta);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log('value of nickname: ' + nickname);
+console.log('typeof nickname: ' + typeof nickname);
