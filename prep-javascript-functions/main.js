7e1f77eb7da9238ded7211277a85e12fcbe3eaed
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2024-11-22 04:20:13 +0000

    Completed prep-javascript-functions exercise

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,62 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+const sum = addTwoNumbers(2, 2);
+console.log(sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+const minutes = convertHoursToMinutes(2);
+console.log(minutes);
+
+function getGreeting(name) {
+  return 'Hey ' + name + '!';
+}
+
+const greeting = getGreeting('Tom');
+console.log(greeting);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+const sumTimesFive = addAndMultiplyBy5(10, 5);
+console.log(sumTimesFive);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+
+const productDividedByFive = multiplyAndDivideBy5(35, 10);
+console.log(productDividedByFive);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+const difference = subtractTwoNumbers(22, 7);
+console.log(difference);
+
+function getCircleCircumference(radius) {
+  return 2 * 3.1416 * radius;
+}
+
+const circumference = getCircleCircumference(5);
+console.log(circumference);
+
+function getFullName(firstName, lastName) {
+  return firstName + lastName;
+}
+
+const fullName = getFullName('Ruqaiya', 'Electricwala');
+console.log(fullName);
+
+function cube(number) {
+  return number * number * number;
+}
+
+const cubed = cube(5);
+console.log(cubed);
